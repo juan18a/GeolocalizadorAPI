c189229f54d97cf60378bb8f44164767bd17f706
juan18a <jcda18a@gmail.com>
2020-07-02 14:28:32 +0000

    subir a github

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+var http = require('http');
+var morgan = require('morgan')
+
+
+app.use(morgan('combined'))
+
+
+app.set('trust proxy', true);
+
+
+app.get('/', (req, res) => {
+
+/*
+http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+  resp.on('data', function(ip) {
+    console.log("My public IP address is: " + ip);
+  });
+})
+
+*/
+console.dir(req.ips)
+
+const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress ||  req.socket.remoteAddress || req.connection.socket.remoteAddress;
+res.send('Your IP is ' + ip);
+
+
+
+});
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server port on 3000");
+})
