23708d1198f9bb3abae85cc78afdcaac887d733d
juan18a <jcda18a@gmail.com>
2020-07-02 16:55:53 +0000

    change header req ip

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
 const express = require('express');
 const app = express();
 var http = require('http');
-var morgan = require('morgan')
+var morgan = require('morgan');
 
 
 app.use(morgan('combined'))
 
-
 app.set('trust proxy', true);
-
-
 app.get('/', (req, res) => {
 
-/*
-http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
-  resp.on('data', function(ip) {
-    console.log("My public IP address is: " + ip);
-  });
-})
 
-*/
 console.dir(req.ips)
 
-const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress ||  req.socket.remoteAddress || req.connection.socket.remoteAddress;
-res.send('Your IP is ' + ip);
+const ip = req.headers['x-forwarded-for'] ||
+ req.connection.remoteAddress;
+
+
+
+ res.send('Your IP is ' + ip);
+
+
 
 
 
